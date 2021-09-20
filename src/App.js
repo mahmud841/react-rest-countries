370f1d1f762c405d2ad73690417466d03f0cb82e
AL MAHMUD SURUJ <mahmudsuruj14@gmail.com>
2021-09-20 15:04:09 +0800

    third commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Countries from './components/Countries/Countries';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
     <Countries></Countries>
+    <Footer></Footer>
     
      
     
