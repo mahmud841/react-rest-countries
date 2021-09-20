18e1c3109ae752d94e81626ed2d638afad1a8be2
AL MAHMUD SURUJ <mahmudsuruj14@gmail.com>
2021-09-20 12:58:24 +0800

    first commit

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
+
+const Countries = () => {
+  const [countries, setCountries] = useState([]);
+  useEffect(() =>{
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    // .then(data => console.log(data))
+    .then(data => setCountries(data))
+  }, [])
+  return (
+    <div>
+      <h1>Hello from China :{countries.length} </h1>
+      {
+        // countries.map(country => console.log(country))
+        countries.map(country => <Country></Country> )
+      }
+    </div>
+  );
+};
+
+export default Countries;
---
+++ src/components/Country/Country.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Country = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h3>This is a Single Country </h3>
+    </div>
+  );
+};
+
+export default Country;
